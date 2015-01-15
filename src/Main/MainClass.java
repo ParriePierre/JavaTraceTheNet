47d93f5392a93e306135cc34be11dda805319330
ataraxg <camgene92500@msn.com>
2015-01-15 14:02:21 +0100

    Recup des donnée du fakeroute

--- src/Main/MainClass.java
+++ src/Main/MainClass.java
@@ -1,13 +1,19 @@
 package Main;
 
+import java.io.IOException;
+import traceRoutePackage.TraceRouteExec;
+
+
 public class MainClass {
 
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException, InterruptedException {
+            char[] ip;
+            TraceRouteExec trace = new TraceRouteExec();
+            ip = trace.getIp();
+            System.out.println(ip[0]);
 	}
 
 }
--- src/traceRoutePackage/TraceRouteExec.java
+++ src/traceRoutePackage/TraceRouteExec.java
@@ -1,38 +1,56 @@
 package traceRoutePackage;
 
+import fr.ece.fauberte.fakeroute.trace.FakeTraceGenerator;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.lang.Process;
 import java.lang.ProcessBuilder;
 
 public class TraceRouteExec {
-	public TraceRouteExec() throws IOException, InterruptedException
-	{
-		//Create a process builder that contains the command and its arguments
-		ProcessBuilder pb = new ProcessBuilder("traceroute","-n","google.com");
-		
-		//Launches the sub process
-		Process p = pb.start();
-		
-		//Get its output stream
-		InputStream in=p.getInputStream();
-		
-		//Wait for the process to end
-		p.waitFor();
-		
-		char buff=' ';
-		int test=0;
+    char[] ip;
+    public TraceRouteExec() throws IOException, InterruptedException {
+        /*FakeTraceGenerator generator = new FakeTraceGenerator("ece.fr");
+        
+        System.out.println(generator.generate());*/
+       
+        
+        //Create a process builder that contains the command and its arguments
+        ProcessBuilder pb = new ProcessBuilder();
+
+        //Launches the sub process
+        Process p =  Runtime.getRuntime().exec("Java -jar fakeroute.jar ece.fr");
+
+        //Get its output stream
+        InputStream in = p.getInputStream();
+
+        //Wait for the process to end
+        p.waitFor();
+        
+        
+        char buff = ' ';
+        int test = 0;
+        int i = 0;
 		//read the output
-			//read the first character
-		test= in.read();
-		buff=(char) test;
-		while(test!=-1)
-		{
-			//print a character
-			System.out.print(buff);
-			//read the next
-			test= in.read();
-			buff=(char) test;
-		}
-	}
+        //read the first character
+        test = in.read();
+        buff = (char) test;
+        
+        ip[i] = buff;
+       
+        while (test != -1) {
+            //print a character
+           // System.out.print(buff);
+            //read the next
+           ip[i] = buff;
+           i++;
+           test = in.read();
+           buff = (char) test;
+        }
+    }
+    
+    public char[] getIp(){
+                return ip;
+    }
 }
