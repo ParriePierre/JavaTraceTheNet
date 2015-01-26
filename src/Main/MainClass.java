e81bcd48d5eeb24816e7b1ed024f8fa167d73f57
ataraxg <camgene92500@msn.com>
2015-01-26 10:07:40 +0100

    Recup des ip

--- src/Main/MainClass.java
+++ src/Main/MainClass.java
@@ -1,19 +1,47 @@
 package Main;
 
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
 import traceRoutePackage.TraceRouteExec;
-
 
 public class MainClass {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) throws IOException, InterruptedException {
-            char[] ip;
-            TraceRouteExec trace = new TraceRouteExec();
-            ip = trace.getIp();
-            System.out.println(ip[0]);
-	}
+    /**
+     * @param args
+     */
+    private static final String IPADDRESS_PATTERN
+            = "(\\*)|"
+            + "(([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\."
+            + "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\."
+            + "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\."
+            + "([01]?\\d\\d?|2[0-4]\\d|25[0-5]))|"
+            + "((\\d\\d\\d)\\.(\\d\\d\\d)|(\\d\\d)\\.(\\d\\d\\d)|(\\d)\\.(\\d\\d\\d))";
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Pattern pattern;
+        Matcher matcher;
+        String ip;
+        String[] ips = null;
+        TraceRouteExec trace = new TraceRouteExec();
+        ip = trace.getIp();
+        int i=0;
+        System.out.println(ip);
+
+        try {
+
+            pattern = Pattern.compile(IPADDRESS_PATTERN);
+            matcher = pattern.matcher(ip);
+            while (matcher.find()) {
+                ips[i]= matcher.group();
+               
+                System.out.println(ips[i]);
+                i++;
+            }
+        } catch (PatternSyntaxException pse) {
+            pse.printStackTrace();
+        }
+    }
 
 }
--- src/traceRoutePackage/TraceRouteExec.java
+++ src/traceRoutePackage/TraceRouteExec.java
@@ -9,48 +9,29 @@ import java.lang.Process;
 import java.lang.ProcessBuilder;
 
 public class TraceRouteExec {
-    char[] ip;
+
+    String chaine = "";
+
     public TraceRouteExec() throws IOException, InterruptedException {
-        /*FakeTraceGenerator generator = new FakeTraceGenerator("ece.fr");
-        
-        System.out.println(generator.generate());*/
-       
-        
-        //Create a process builder that contains the command and its arguments
-        ProcessBuilder pb = new ProcessBuilder();
 
         //Launches the sub process
-        Process p =  Runtime.getRuntime().exec("Java -jar fakeroute.jar ece.fr");
+        Process p = Runtime.getRuntime().exec("Java -jar fakeroute.jar ece.fr");
 
         //Get its output stream
         InputStream in = p.getInputStream();
 
         //Wait for the process to end
         p.waitFor();
-        
-        
-        char buff = ' ';
-        int test = 0;
-        int i = 0;
-		//read the output
-        //read the first character
-        test = in.read();
-        buff = (char) test;
-        
-        ip[i] = buff;
-       
-        while (test != -1) {
-            //print a character
-           // System.out.print(buff);
-            //read the next
-           ip[i] = buff;
-           i++;
-           test = in.read();
-           buff = (char) test;
+        InputStreamReader ipsr = new InputStreamReader(in);
+        BufferedReader br = new BufferedReader(ipsr);
+        String ligne;
+        while ((ligne = br.readLine()) != null) {
+            chaine += ligne + "\n";
         }
+
     }
-    
-    public char[] getIp(){
-                return ip;
+
+    public String getIp() {
+        return chaine;
     }
 }
